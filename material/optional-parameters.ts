61c4363b7450fa171537bdf91af8a62a96e5ad56
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-04 09:23:39 -0300

    Arrays & Tuples

--- material/optional-parameters.ts
+++ material/optional-parameters.ts
@@ -3,3 +3,11 @@ function greet(name?: string) {
  }
   
  greet(); // Prints: Hello, Anonymous!
+
+
+let dinner: string[] = ['Chicken Pot Pie', 'Brisket', 'Spaghetti'];
+let lunch: string[] = ['Sandwich', 'Soup', 'Bread']
+
+let todaysMenu: string[][] = [['Cereal', 'Toast'] , lunch];
+console.log(todaysMenu[1]);
+console.log(...lunch);
